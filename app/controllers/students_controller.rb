486b1922ae5623fe3b0d73ff4314d14639465de9
smh1988-2 <seamhurley1988@gmail.com>
2022-01-11 00:39:00 -0800

    commit tree 1d266a743beedec4643b3adb2f0982eeee1b0ed1

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    # render json: students
+
+    students = if params[:name]
+      Student.find_by(first_name: params[:name])
+    else
+      Student.all
+    end    
+render json: students
+  end
+
+  def show
+    student = Student.find_by_id(params[:id])
+    render json: student
   end
 
 end
